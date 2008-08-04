028473549c0b84ab3184cab52ea04d17619b02f3
june.tate <june.tate@3cbbdd56-3241-0410-ba23-a9dc416a0590>
2008-08-04 03:51:38 +0000

    Adding in some more functional tests, and cleaning them up a bit.
    git-svn-id: http://tsumufs.googlecode.com/svn/trunk@150 3cbbdd56-3241-0410-ba23-a9dc416a0590

--- tests/functional/open.c
+++ tests/functional/open.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2008  Google, Inc. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/xattr.h>
@@ -8,47 +26,194 @@
 #include <errno.h>
 #include <string.h>
 
-int test_create()
+
+const char *g_testfilename = "this.file.shouldnt.exist";
+
+struct test_matrix {
+    mode_t mode;
+    int    errno_result;
+};
+
+int test_open_enoent_failures()
 {
-    int fd = open("test/testme.txt", O_CREAT | O_RDWR, 0644);
+    int idx = 0;
+    int fd  = 0;
+    int old_errno = 0;
+
+    struct test_matrix tests[] = {
+        {O_RDONLY,          ENOENT},
+        {O_WRONLY,          ENOENT},
+        {O_RDONLY|O_WRONLY, ENOENT},
+        {O_RDONLY|O_EXCL,   ENOENT},
+        {O_WRONLY|O_EXCL,   ENOENT},
+        {O_RDWR|O_EXCL,     ENOENT},
+        {-1,                -1}
+    };
+
+    printf("%s [", __func__);
+    fflush(stdout);
+
+    for (idx = 0; tests[idx].mode != -1; idx++) {
+        fd = open(g_testfilename, tests[idx].mode);
+        old_errno = errno;
+
+        if (fd < 0) {
+            if (errno == tests[idx].errno_result) {
+                printf(".");
+                fflush(stdout);
+                continue;
+            }
+        }
+
+        printf("] fail!\n");
+        fprintf(stderr, "Test index %d in %s failed.\n", idx, __func__);
+        fprintf(stderr, "Errno %d: %s\n", old_errno, strerror(old_errno));
+
+        return 0;
+    }
+
+    printf("] ok!\n");
+    fflush(stdout);
+    return 1;
+}
+
+int test_open_exist()
+{
+    int idx = 0;
+    int fd  = 0;
+    int old_errno = 0;
+
+    struct test_matrix tests[] = {
+        {O_RDONLY,        0},
+        {O_WRONLY,        0},
+        {O_RDWR,          0},
+        {O_RDONLY|O_EXCL, 0},
+        {O_WRONLY|O_EXCL, 0},
+        {O_RDWR|O_EXCL,   0},
+        {-1,              -1}
+    };
+
+    printf("%s [", __func__);
+    fflush(stdout);
+
+    fd = open(g_testfilename, O_RDWR|O_CREAT, 0644);
 
     if (fd < 0) {
-        perror("Unable to open testme.txt for writing");
+        printf("] fail!\n");
+        fprintf(stderr, "Test preparation in %s failed.\n", __func__);
+        fprintf(stderr, "Errno %d: %s\n", errno, strerror(errno));
         return 0;
     }
 
     if (close(fd) < 0) {
-        perror("Unable to close testme.txt");
+        printf("] fail!\n");
+        fprintf(stderr, "Test preparation in %s failed.\n", __func__);
+        fprintf(stderr, "Errno %d: %s\n", errno, strerror(errno));
         return 0;
     }
 
+    for (idx = 0; tests[idx].mode != -1; idx++) {
+        fd = open(g_testfilename, tests[idx].mode);
+        old_errno = errno;
+
+        if (fd > 0) {
+            printf(".");
+            fflush(stdout);
+
+            close(fd);
+            continue;
+        }
+
+        printf("] fail!\n");
+        fprintf(stderr, "Test index %d in %s failed.\n", idx, __func__);
+        fprintf(stderr, "Errno %d: %s\n", old_errno, strerror(old_errno));
+
+        return 0;
+    }
+
+    printf("] ok!\n");
+    fflush(stdout);
+    return 1;
+}
+
+int test_open_create()
+{
+    int idx = 0;
+    int fd  = 0;
+    int old_errno = 0;
+
+    struct test_matrix tests[] = {
+        {O_RDONLY|O_CREAT,         0},
+        {O_WRONLY|O_CREAT,         0},
+        {O_RDWR|O_CREAT,           0},
+        {O_RDONLY|O_CREAT|O_EXCL,  0},
+        {O_WRONLY|O_CREAT|O_EXCL,  0},
+        {O_RDWR|O_CREAT|O_EXCL,    0},
+        {-1,                      -1}
+    };
+
+    printf("%s [", __func__);
+    fflush(stdout);
+
+    for (idx = 0; tests[idx].mode != -1; idx++) {
+        fd = open(g_testfilename, tests[idx].mode);
+        old_errno = errno;
+
+        if (fd > 0) {
+            printf(".");
+            fflush(stdout);
+
+            close(fd);
+            unlink(g_testfilename);
+            continue;
+        }
+
+        printf("] fail!\n");
+        fprintf(stderr, "Test index %d in %s failed.\n", idx, __func__);
+        fprintf(stderr, "Errno %d: %s\n", old_errno, strerror(old_errno));
+
+        return 0;
+    }
+
+    printf("] ok!\n");
+    fflush(stdout);
     return 1;
 }
 
 int test_create_already_exists()
 {
-    int fd = open("test/testme.txt", O_CREAT | O_RDWR, 0644);
-    int fd2 = open("test/testme.txt", O_CREAT | O_EXCL | O_RDWR, 0644);
+    int fd  = open(g_testfilename, O_CREAT | O_RDWR, 0644);
+    int fd2 = open(g_testfilename, O_CREAT | O_EXCL | O_RDWR, 0644);
+
+    printf("%s [.", __func__);
+    fflush(stdout);
 
     if (fd < 0) {
+        printf("] fail!\n");
         perror("Unable to open testme.txt for writing");
         return 0;
     }
 
-    if (fd > 0) {
+    if (fd2 > 0) {
+        printf("] fail!\n");
         perror("Second open did not return an error");
         return 0;
     }
 
     if (errno != EEXIST) {
+        printf("] fail!\n");
         perror("Second open did not return EEXIST");
         return 0;
     }
 
     if (close(fd) < 0) {
-        perror("Unable to close testme.txt");
+        printf("] fail!\n");
+        perror("Unable to close fd");
         return 0;
     }
+
+    printf("] ok!\n");
+    fflush(stdout);
 
     return 1;
 }
@@ -56,17 +221,15 @@ int test_create_already_exists()
 int connected(void)
 {
     char *test_str = "1";
-    char *buf = "   ";
+    char buf[2] = " \0";
     int size = 0;
 
-    size = getxattr("test", "tsumufs.connected", buf, strlen(buf));
+    size = getxattr(".", "tsumufs.connected", buf, strlen(buf));
 
     if (size == -1) {
-        perror("Unable to getxattr tsumufs.connected from test");
+        perror("Unable to getxattr tsumufs.connected from current directory");
         exit(1);
     }
-
-    printf("Got '%s', wanted '%s'\n", buf, test_str);
 
     if (strcmp(buf, test_str) == 0) {
         return 1;
@@ -77,13 +240,16 @@ int connected(void)
 
 int main(void)
 {
-    sleep(5);
     while (!connected()) {
         printf("Waiting for tsumufs to mount.\n");
         sleep(1);
     }
+    printf("Mounted.\n");
+    sleep(1);
 
-    if (!test_create()) return 1;
+    if (!test_open_enoent_failures()) return 1;
+    if (!test_open_create()) return 1;
+    if (!test_open_exist()) return 1;
     if (!test_create_already_exists()) return 1;
 
     return 0;
--- tests/functional/write.c
+++ tests/functional/write.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2008  Google, Inc. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -32,7 +50,7 @@ int test_single_write(void)
 
         total += result;
     }
-    
+
     if (close(fd) < 0) {
         printf("Failed at %d\n", __LINE__);
         perror("Unable to close testme.txt");
@@ -41,7 +59,7 @@ int test_single_write(void)
 
     return 1;
 }
-        
+
 int test_multiple_writes(void)
 {
     char *output = "Zorba!\n";
@@ -60,21 +78,21 @@ int test_multiple_writes(void)
     for (i=0; i<maxcount; i++) {
         total = 0;
         result = 0;
-        
+
         while (total < strlen(output)) {
             result = write(fd, output + result, strlen(output));
-            
+
             if (result < 0) {
                 printf("Failed at %d\n", __LINE__);
                 perror("Unable to write to file testme.txt");
                 close(fd);
                 return 0;
             }
-            
+
             total += result;
         }
     }
-    
+
     if (close(fd) < 0) {
         printf("Failed at %d\n", __LINE__);
         perror("Unable to close testme.txt");
@@ -82,7 +100,7 @@ int test_multiple_writes(void)
     }
 
     return 1;
-}    
+}
 
 int main(void)
 {
