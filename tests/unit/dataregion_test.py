a4659e3c67aa25f22a79aabab2e9aaacf64a3321
june.tate <june.tate@3cbbdd56-3241-0410-ba23-a9dc416a0590>
2009-01-13 05:23:57 +0000

    Adding in a whole new set of unit tests to make sure DataRegions are sane during merges.
    git-svn-id: http://tsumufs.googlecode.com/svn/trunk@298 3cbbdd56-3241-0410-ba23-a9dc416a0590

--- tests/unit/dataregion_test.py
+++ tests/unit/dataregion_test.py
@@ -44,22 +44,72 @@ class InstanceCheck(unittest.TestCase):
 
 class OverlapCheck(unittest.TestCase):
   def setUp(self):
-    self.r1 = None
-    self.r1Start = 10
-    self.r1End   = 100
-    self.overlappingRegions = [
-      { 'start': 50,  'end': 150 },  # overlap to right
-      { 'start': 10,  'end': 100 },  # exact overlap
-      { 'start': 0,   'end': 150 },  # complete overlap
-      { 'start': 10,  'end': 50 },   # overlap to left, exact on left
-      { 'start': 50,  'end': 100},   # overlap to right, exact on right
-      { 'start': 100, 'end': 1000},  # overlap to right,
+    self.r1Start = 5
+    self.r1End   = 10
+
+    self.regionResults = [               # 012345678901234567890
+      ('2' * 5),                         #      22222
+      ('2' * 15),                        # 222222222222222
+      ('1') + ('2' * 3) + ('1'),         #      12221
+      ('2' * 5) + ('1' * 5),             # 2222211111
+      ('2' * 6) + ('1' * 4),             # 2222221111
+      ('2' * 8) + ('1' * 2),             # 2222222211
+      ('2' * 10),                        # 2222222222
+      ('2' * 10),                        #      2222222222
+      ('1' * 2) + ('2' * 8),             #      1122222222
+      ('1' * 4) + ('2' * 6),             #      1111222222
+      ('1' * 5) + ('2' * 5)              #      1111122222
       ]
 
-    self.r1 = dataregion.DataRegion(self.r1Start,
-                                    self.r1End,
-                                    '1' * (self.r1End - self.r1Start))
+    self.overlappingRegions = [
+      { 'start': 5,  'end': 10, 'type': 'perfect-overlap' },
+      { 'start': 0,  'end': 15, 'type': 'outer-overlap'   },
+      { 'start': 6,  'end': 9,  'type': 'inner-overlap'   },
+      { 'start': 0,  'end': 5,  'type': 'left-adjacent'   },
+      { 'start': 0,  'end': 6,  'type': 'left-overlap'    },  # overlap to left, exact on left
+      { 'start': 0,  'end': 8,  'type': 'left-overlap'    },  # overlap to left
+      { 'start': 0,  'end': 10, 'type': 'left-overlap'    },  # overlap to left, exact on right
+      { 'start': 5,  'end': 15, 'type': 'right-overlap'   },  # overlap to right, exact on left
+      { 'start': 7,  'end': 15, 'type': 'right-overlap'   },  # overlap to right
+      { 'start': 9,  'end': 15, 'type': 'right-overlap'   },  # overlap to right, exact on right
+      { 'start': 10, 'end': 15, 'type': 'right-adjacent'  }
+      ]
 
+  def testMergeTypes(self):
+    for i in range(0, len(self.overlappingRegions)):
+      testcase = self.overlappingRegions[i]
+      result   = self.regionResults[i]
+
+      r1 = dataregion.DataRegion(self.r1Start, self.r1End,
+                                 '1' * (self.r1End - self.r1Start))
+      r2 = dataregion.DataRegion(testcase['start'], testcase['end'],
+                                 '2' * (testcase['end'] - testcase['start']))
+
+      merge_type = r1.canMerge(r2)
+
+      self.assertEqual(testcase['type'], merge_type)
+
+  def testMerges(self):
+    for i in range(0, len(self.overlappingRegions)):
+      testcase = self.overlappingRegions[i]
+      result   = self.regionResults[i]
+
+      r1 = dataregion.DataRegion(self.r1Start, self.r1End,
+                                 '1' * (self.r1End - self.r1Start))
+      r2 = dataregion.DataRegion(testcase['start'], testcase['end'],
+                                 '2' * (testcase['end'] - testcase['start']))
+
+      r3 = r1.mergeWith(r2)
+
+      self.assertEqual(result, r3.getData())
+
+  def testNonMergable(self):
+    for testcase in self.overlappingRegions:
+      r1 = dataregion.DataRegion(2000, 2001, '1')
+      r2 = dataregion.DataRegion(testcase['start'], testcase['end'],
+                                 '2' * (testcase['end'] - testcase['start']))
+
+      self.assertEqual(None, r1.canMerge(r2))
 
 if __name__ == '__main__':
   unittest.main()
